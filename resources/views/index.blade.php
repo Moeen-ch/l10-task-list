d824c1d03ae0f8a5cccb6710030892344f86c1c6
Moeen-ch <muhammadmoeen109@gmail.com>
2025-01-22 22:51:38 +0500

    add pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,18 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-@forelse ($tasks as $task )
-        <div>
-            <ul>
-                <li>
-                    <a href="{{ route('tasks.show',['task'=>$task->id]) }}">{{ $task -> title }}</a>
-                </li>
-            </ul>
+    @forelse ($tasks as $task)
+        <div style="margin-left: 10px">
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
         </div>
     @empty
-    <div>there are a not tasks </div>
+        <div>there are a not tasks </div>
     @endforelse
+
+
+    @if ($tasks->count())
+        <div>
+            {{ $tasks->links() }}
+        </div>
+    @endif
 @endsection
-
-
-
