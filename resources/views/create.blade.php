7f2a027161e16b8846c7fe1e73255795a34cdc0c
Moeen-ch <muhammadmoeen109@gmail.com>
2025-01-22 11:49:34 +0500

    keeping old values in the form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -14,21 +14,21 @@
     @csrf
     <div>
         <label for="title">Title</label> <br>
-        <input type="text" name="title" id="title">
+        <input type="text" name="title" id="title" value="{{ old('title') }}">
         @error('title')
           <p class="error-message">{{ $message }}</p>  
         @enderror
     </div>
     <div>
         <label for="description">Description</label> <br>
-        <textarea name="description" id="description" rows="5" cols="23" ></textarea>
+        <textarea name="description" id="description" rows="5" cols="23">{{ old('description') }}</textarea>
         @error('description')
           <p class="error-message">{{ $message }}</p>  
         @enderror
     </div>
     <div>
         <label for="long_description">Long description</label> <br>
-        <textarea name="long_description" id="long_description" rows="10" cols="23"></textarea>
+        <textarea name="long_description" id="long_description" rows="10" cols="23">{{ old('long_description') }}</textarea>
         @error('long_description')
           <p class="error-message">{{ $message }}</p>  
         @enderror
