a9f13685269b83e71b38251b9c30c9085a6878c0
Moeen-ch <muhammadmoeen109@gmail.com>
2025-01-22 11:16:14 +0500

    Sessin, errors and flash messages

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,20 +2,36 @@
 
 @section('title', 'Add a new task')
 
+@section('styles')
+<style>
+    .error-message{
+        color:red;
+        fount-size: 0.8rem;
+    }
+</style>
 @section('content')
 <form method="POST" action="{{ route('tasks.store') }}">
     @csrf
     <div>
         <label for="title">Title</label> <br>
         <input type="text" name="title" id="title">
+        @error('title')
+          <p class="error-message">{{ $message }}</p>  
+        @enderror
     </div>
     <div>
         <label for="description">Description</label> <br>
         <textarea name="description" id="description" rows="5" cols="23" ></textarea>
+        @error('description')
+          <p class="error-message">{{ $message }}</p>  
+        @enderror
     </div>
     <div>
         <label for="long_description">Long description</label> <br>
         <textarea name="long_description" id="long_description" rows="10" cols="23"></textarea>
+        @error('long_description')
+          <p class="error-message">{{ $message }}</p>  
+        @enderror
     </div>
     <div>
         <button type="submit">Add task</button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <title>Larave 10-task-list</title>
+    @yield('styles')
 </head>
 
 <body>
     <h1>@yield('title')</h1>
+
+    @if (session()->has('success'))
+        <div style="color: blue">{{ session('success') }}</div>     
+    @endif
+
     <div>
         @yield('content')
     </div>
