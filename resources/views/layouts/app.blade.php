728a0618ffee8c5f0a96e7fc2b445f1b9796a1f6
Moeen-ch <muhammadmoeen109@gmail.com>
2025-01-23 17:03:40 +0500

    Stylish flash messages and using Alphine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
     <title>Larave 10-task-list</title>
     {{-- blade-formater-disable --}}
     {{-- <style type="text/tailwindcss">
@@ -19,12 +20,28 @@
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
     <h1 class=" mb-4 text-2xl">@yield('title')</h1>
 
-    @if (session()->has('success'))
-        <div style="color: blue">{{ session('success') }}</div>     
-    @endif
+    <div x-data="{flash : true}">
+        @if (session()->has('success'))
+            {{-- <div style="color: blue"></div> --}}
+            <div x-show="flash" class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+                role="alert">
+                <strong class="font-bold">Success!</strong>
+                <div>{{ session('success') }}</div>
 
-    <div>
-        @yield('content')
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                      stroke-width="1.5" @click="flash = false"
+                      stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </span>
+            </div>
+            {{-- vector graphic element svg --}}
+        @endif
+
+        
+            @yield('content')
+        
     </div>
 </body>
 
